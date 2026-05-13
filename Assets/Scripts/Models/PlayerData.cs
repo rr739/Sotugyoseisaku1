9fbc73858f0546fe2198610fecab2294677c1d9b
GM2140017\ydauser01 <ryuki28630@gmail.com>
2026-05-13 15:32:11 +0900

    ロビーでの他プレイヤーの名前表示追加

--- Assets/Scripts/Models/PlayerData.cs
+++ Assets/Scripts/Models/PlayerData.cs
@@ -5,7 +5,8 @@ public class InitResponse
     public string type;
     public string name_id;  
     public string room_id;     // Š‘®‚·‚éƒ‹[ƒ€ID
-    public int index;
+    public int  index;
+    public bool IsStarted;
 }
 
 [Serializable]
